82e30ddf20aa34194c92a400738858c1885a0127
johnrpo-dev <johnsod8729@hotmail.com>
2021-09-17 09:58:18 -0500

    se crea el switch

--- src/empleado/Empleado.java
+++ src/empleado/Empleado.java
@@ -10,5 +10,24 @@ package empleado;
  * @author johns
  */
 public class Empleado {
-    
+    int id;
+    String nombre;
+    String cargo;
+    int salario;
+    String telefono;
+    String direccion;
+
+    public Empleado() {
+        
+    }
+
+    public Empleado(int id, String nombre, String cargo, int salario, String telefono, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
 }
+    
